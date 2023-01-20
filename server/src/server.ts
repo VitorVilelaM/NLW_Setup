0f7c124b267d5d747316618453942ea9b8608c23
Vitor Vilela <vilelavitor305@gmail.com>
2023-01-20 19:19:33 -0300

    Finalizando Aula 2
    Novas tabelas no banco de dados, novos relacionamentos e novas querys. Na parte do back-end foram feitas novas rotas de Post e Get. e No front end e mobile, criando a tela de Home.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,12 @@
 //Back-End : API - RESTfull
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { appRoutes } from "./routes";
 
-const app = Fastify(); // Criaçao da Aplicação para utilização de Rotas
-const prisma = new PrismaClient(); // FrameWork para utilização do BD
+export const app = Fastify(); // Criaçao da Aplicação para utilização de Rotas
 app.register(cors); // Permissão para que o FrontEnd possa se comunicar com os dados do BD
-
-// Função da Página principal da aplicação
-app.get('/', async ()=>{
-  const habits = await prisma.habit.findMany(); // Fazendo um Select no BD, utilizando o Prisma
-  return habits; // retornando objetos(tabelas do BD) na tela após o select
-});
+app.register(appRoutes) //Utilizando um método do Fastify, para subdivisão do código
+ 
 
 // Função para manter o servidor na porta 3333
 app.listen({
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
 import './styles/global.css'
-import { Habit } from './components/habit'
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export function App() {
   return (
-    <Habit completed={2} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
+    </div>
   )
 }
-
-export default App
