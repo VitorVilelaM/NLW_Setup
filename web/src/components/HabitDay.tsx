22f0458b8cf51c39ad0a858400a1e254f6441b96
Vitor Vilela <vilelavitor305@gmail.com>
2023-01-21 01:19:12 -0300

    Metade da Aula 3
    Colocando novos componentes no Front-Wend e funcionalidades ao botão de novo-hábito e, popovers e cores em cada quadradinho.

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,11 +1,42 @@
-/*
-interface HabitDayProps{
-  completed: number
-}
-*/
+import * as Popover from '@radix-ui/react-popover';
+import clsx from 'clsx';
+import { ProgressBar } from './ProgressBar';
 
-export function HabitDay(){
+
+interface HabitDayProps {
+  completed: number,
+  amount: number
+}
+
+
+export function HabitDay(props : HabitDayProps) {
+
+  const completedPorcetagem = Math.round((props.completed/props.amount) * 100); 
+
   return (
-    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+    <Popover.Root>
+      <Popover.Trigger 
+        className={clsx('w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg', {
+          'bg-zinc-900 border-zinc-800' : completedPorcetagem === 0,
+          'bg-violet-900 border-violet-800' : completedPorcetagem >= 0 && completedPorcetagem <= 20,
+          'bg-violet-800 border-violet-700' : completedPorcetagem >= 20 && completedPorcetagem <= 40,
+          'bg-violet-700 border-violet-600' : completedPorcetagem >= 40 && completedPorcetagem <= 60,
+          'bg-violet-600 border-violet-500' : completedPorcetagem >= 60 && completedPorcetagem <= 80,
+          'bg-violet-500 border-violet-400' : completedPorcetagem >= 80,
+        })} 
+
+      />
+
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-800 flex flex-col">
+          <span className="font-semibold text-zinc-400">terça-feira</span>
+          <span className="mt-1 font-extrabold leading-tight">17/01</span>
+
+          <ProgressBar progress={completedPorcetagem} />
+
+          <Popover.Arrow height={8} width={16} clasName="fill-zinc-900" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   )
 }
